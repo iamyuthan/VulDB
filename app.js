68d31588304baf6cc947919b030fa127092fd8bf
Yuthan K <83505381+iamyuthan@users.noreply.github.com>
2023-01-11 12:14:59 +1100

    Update app.js

--- app.js
+++ app.js
@@ -34,30 +34,45 @@ d3.csv("vuldb.csv").then(function (data) {
       // console.log(output[i]['original_title'])
       // console.log(output[i]['avg_vote'])
       // d3.select("tbody>tr>td").text(output[i]['original_title']);
-      d3.select("tbody").insert("tr").attr("class","accordion-item").html("<td>"+[i+1]+"</td>"+"<td class=\"d-flex justify-content-between flex-wrap\">"+"<button class=\"btn btn-primary\" type=\"button\" data-bs-toggle=\"modal\" data-bs-target=\"#"+[i+1]+"ModalFullscreen\">"+(output[i]['vulnerability'])+"</button>"
-      + "</td>" +"<td>" +(output[i]['category'])+"</td>"+"<td>" +(output[i]['severity'])+"</td>"+"<td>" +(output[i]['database'])+"</td>"+"<td>" +(output[i]['source'])+"</td>"+"<td>" 
-	  
-	  +"<div id=\""+[i+1]+"ModalFullscreen\" class=\"modal fade\" aria-labelledby=\""+[i+1]+"ModalFullscreenLabel\" aria-hidden=\"true\">"+"<div class=\"modal-dialog modal-dialog-centered modal-dialog-scrollable\">"+"<div class=\"modal-content\">"+"<div class=\"modal-header\">"+"<h5 class=\"modal-title h4\" id=\""+[i+1]+"ModalFullscreenLabel\"> "+(output[i]['vulnerability'])+"</h5>"+"<button type=\"button\" class=\"btn-close\" data-bs-dismiss="modal" aria-label=\"Close\"></button>"+"</div>"+"<div class=\"modal-body\">"+"
-	  +"<p><em><u><strong>Description:</strong></u></em>"+"<br>"
-	  +(output[i]['description'])+"</p>"
-	  +"<p><em><u><strong>Impact:</strong></u></em>"+"<br>"
-	  +(output[i]['impact'])+"</p>"
-	  +"<p><em><u><strong>Likelihood:</strong></u></em>"+"<br>"
-	  +(output[i]['likelihood'])+"</p>"
-	  +"<p><em><u><strong>Remediation:</strong></u></em>"+"<br>"
-	  +(output[i]['remediation'])+"</p>"
-	  +"<p><em><u><strong>Reference:</strong></u></em>"+"<br>"
-	  +(output[i]['reference'])+"</p>"
-	  +"<p><em><u><strong>Severity:</strong></u></em>"+"<br>"
-	  +(output[i]['severity'])+"</p>"
-	  +"<p><em><u><strong>Rating:</strong></u></em>"+"<br>"
-	  +(output[i]['rating'])+"</p>"
-	  +"<p><em><u><strong>CVSS:</strong></u></em>"+"<br>"
-	  +(output[i]['cvss'])+"</p>"
-	  +"<p><em><u><strong>Database:</strong></u></em>"+"<br>"
-	  +(output[i]['database'])+"</p>"
-	  +"<p><em><u><strong>Source:</strong></u></em>"+"<br>"
-	  +(output[i]['source'])+"</p>"+"</div>"+"<div class=\"modal-footer\">"+"<button type=\"button\" class=\"btn btn-secondary\" data-bs-dismiss=\"modal\">Close</button>"+"</div>"+"</div>"+"</div>"+"</div>"+"</td>")
+      d3.select("tbody").insert("tr").attr("class","accordion-item").html("<td>"+[i+1]+"</td>"
+									  +"<td class=\"d-flex justify-content-between flex-wrap\">"
+									  +"<button class=\"btn btn-primary\" type=\"button\" data-bs-toggle=\"modal\" data-bs-target=\"#"
+									  +[i+1]+"ModalFullscreen\">"+(output[i]['vulnerability'])
+									  +"</button>"
+									  +"</td>"
+									  +"<td>" +(output[i]['category'])+"</td>"
+									  +"<td>" +(output[i]['severity'])+"</td>"
+									  +"<td>" +(output[i]['database'])+"</td>"
+									  +"<td>" +(output[i]['source'])+"</td>"
+									  +"<td>"
+									  +"<div id=\""+[i+1]+"ModalFullscreen\" class=\"modal fade\" aria-labelledby=\""+[i+1]
+									  +"ModalFullscreenLabel\" aria-hidden=\"true\">"
+									  +"<div class=\"modal-dialog modal-dialog-centered modal-dialog-scrollable\">"
+									  +"<div class=\"modal-content\">"
+									  +"<div class=\"modal-header\">"
+									  +"<h5 class=\"modal-title h4\" id=\""+[i+1]+"ModalFullscreenLabel\">"+(output[i]['vulnerability'])
+									  +"</h5>"+
+									  "<button type=\"button\" class=\"btn-close\" data-bs-dismiss="modal" aria-label=\"Close\"></button>"
+									  +"</div>"
+									  +"<div class=\"modal-body\">"
+									  +"<p><em><u><strong>Description:</strong></u></em>"+"<br>"+(output[i]['description'])+"</p>"
+	      +"<p><em><u><strong>Impact:</strong></u></em>"+"<br>"+(output[i]['impact'])+"</p>"
+	      +"<p><em><u><strong>Likelihood:</strong></u></em>"+"<br>"+(output[i]['likelihood'])+"</p>"
+	      +"<p><em><u><strong>Remediation:</strong></u></em>"+"<br>"+(output[i]['remediation'])+"</p>"
+	      +"<p><em><u><strong>Reference:</strong></u></em>"+"<br>"+(output[i]['reference'])+"</p>"
+	      +"<p><em><u><strong>Severity:</strong></u></em>"+"<br>"+(output[i]['severity'])+"</p>"
+	      +"<p><em><u><strong>Rating:</strong></u></em>"+"<br>"+(output[i]['rating'])+"</p>"
+	      +"<p><em><u><strong>CVSS:</strong></u></em>"+"<br>"+(output[i]['cvss'])+"</p>"
+	      +"<p><em><u><strong>Database:</strong></u></em>"+"<br>"+(output[i]['database'])+"</p>"
+	      +"<p><em><u><strong>Source:</strong></u></em>"+"<br>"+(output[i]['source'])+"</p>"
+	      +"</div>"
+	      +"<div class=\"modal-footer\">"
+	      +"<button type=\"button\" class=\"btn btn-secondary\" data-bs-dismiss=\"modal\">Close</button>"
+	      +"</div>"
+	      +"</div>"
+	      +"</div>"
+	      +"</div>"
+	      +"</td>")
     }  };
   window.resizeTo(screen.width,screen.height)
 
